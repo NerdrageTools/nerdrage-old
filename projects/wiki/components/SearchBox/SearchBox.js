a88f9c3fe73dec4183bb6d43a9cd4a05e4351bd5
Troy Alford <troy@troyalford.com>
2018-11-11 23:51:48 -0800

    SearchBox campaign theming support

--- projects/wiki/components/SearchBox/SearchBox.js
+++ projects/wiki/components/SearchBox/SearchBox.js
@@ -1,14 +1,20 @@
 import SearchIcon from 'core/icons/search.svg'
-import React from 'react'
+import React, { Component } from 'react'
+import Campaign from '@/contexts/Campaign'
 import './SearchBox.scss'
 
-export default function SearchBox({
-  onSearch,
-}) {
-  return (
-    <div className="search-box">
-      <SearchIcon className="search icon" />
-      <input type="text" defaultValue="" onChange={onSearch} />
-    </div>
-  )
+export default class SearchBox extends Component {
+  static contextType = Campaign
+
+  render() {
+    const { onSearch } = this.props
+    const { theme } = this.context
+
+    return (
+      <div className="search-box" style={{ color: theme.foreground }}>
+        <SearchIcon className="search icon" />
+        <input type="text" defaultValue="" onChange={onSearch} />
+      </div>
+    )
+  }
 }
