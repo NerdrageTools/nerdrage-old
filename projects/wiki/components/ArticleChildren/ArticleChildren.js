8d6ab7770f7bc0b1e0c60b01dd13e3349e3532d4
Troy Alford <troy@troyalford.com>
2018-11-12 16:12:10 -0800

    Add ArticleChildren component

---
+++ projects/wiki/components/ArticleChildren/ArticleChildren.js
@@ -0,0 +1,29 @@
+import TagIcon from 'core/icons/tag.svg'
+import React from 'react'
+import ArticleLink from '@/components/ArticleLink'
+import './ArticleChildren.scss'
+
+const ArticleChildren = ({
+  articles = [],
+  caption = 'Child Articles',
+  className = '',
+  icon = <TagIcon />,
+}) => {
+  if (!articles.length) return <div className="tag-browser is-hidden" />
+
+  const links = articles
+    .sort((a, b) => a.displayName.localeCompare(b.displayName))
+    .map(({ slug, displayName }) => (
+      <ArticleLink key={slug} to={`/page/${slug}`}>{displayName}</ArticleLink>
+    ))
+
+  return (
+    <div className={`article-children ${className}`.trim()}>
+      <header>{icon} {caption}</header>
+      <div className="link-list">{links}</div>
+    </div>
+  )
+}
+
+ArticleChildren.displayName = 'ArticleChildren'
+export default ArticleChildren
