a6346f92660cd89575d6297c159d46c57cdbde9a
Troy Alford <troy@troyalford.com>
2018-11-12 16:13:09 -0800

    Article tabbed layout

---
+++ projects/wiki/components/TabSet/TabSet.js
@@ -0,0 +1,51 @@
+import React, { Fragment } from 'react'
+import './TabSet.scss'
+
+const DEFAULT_TAB = {
+  id: 'blank',
+  onClick: () => {},
+  renderContents: () => <div />,
+  renderTab: () => <div />,
+}
+
+const TabSet = ({ activeTabId, buttons, onTabClicked = noop, showTabs = true, tabs = [] }) => {
+  if (tabs.length === 0) tabs.push(DEFAULT_TAB)
+
+  const activeId = activeTabId || tabs[0].id
+  const activeTab = tabs.find(tab => tab.id === activeId) || tabs[0]
+  const className = [
+    'tab-set',
+    !showTabs ? 'no-tabs' : '',
+  ].filter(Boolean).join(' ')
+
+  return (
+    <div className={className}>
+      {showTabs && (
+      <Fragment>
+        <ul className={`tabs ${activeTabId}`}>
+          {tabs.map((tab) => {
+            // eslint-disable-next-line no-param-reassign
+            if (!tab.onClick) tab.onClick = () => onTabClicked(tab.id)
+
+            const liClassName = [
+              'tab',
+              tab.className,
+              tab.id,
+              activeTab.id === tab.id && 'is-active',
+            ].filter(Boolean).join(' ')
+
+            return <li key={tab.id} className={liClassName} onClick={tab.onClick}>{tab.tab}</li>
+          })}
+          <li className="buttons">{buttons}</li>
+        </ul>
+      </Fragment>
+      )}
+      <div className="tab-contents">
+        {activeTab.contents}
+      </div>
+    </div>
+  )
+}
+
+TabSet.displayName = 'TabSet'
+export default TabSet
--- projects/wiki/pages/article.js
+++ projects/wiki/pages/article.js
@@ -1,19 +1,72 @@
+import EditIcon from 'core/icons/edit.svg'
+import HtmlIcon from 'core/icons/html.svg'
+import ReadIcon from 'core/icons/read.svg'
 import fetch from 'isomorphic-unfetch'
+import dynamic from 'next/dynamic'
 import React, { Component } from 'react'
 import JsxParser from 'react-jsx-parser'
+import ArticleChildren from '@/components/ArticleChildren'
+import TabSet from '@/components/TabSet'
 import URI from '@/utilities/URI'
+import './article.scss'
+
+let HtmlEditor = () => <div />
+let WysiwygEditor = () => <div />
+
+if (process.browser && window) {
+  HtmlEditor = dynamic(() => import('@/components/HtmlEditor'))
+  WysiwygEditor = dynamic(() => import('@/components/WysiwygEditor'))
+}
 
 export default class Article extends Component {
+  static defaultProps = {
+    children: [],
+    html: '',
+    title: '',
+  }
   static getInitialProps = async ({ query, req }) => (
     fetch(URI(req, `/api/wiki/${query.slug}`)).then(r => r.json())
   )
 
+  state = {
+    activeTab: 'read',
+  }
+
+  handleChangeHtml = (html) => {
+
+  }
+  handleTabClicked = (tab) => {
+    if (tab !== this.state.activeTab) { this.setState({ activeTab: tab }) }
+  }
+
   render() {
-    const { html, title } = this.props
+    const { children, html, title } = this.props
+    const { activeTab } = this.state
+
     return (
       <div className="article page">
-        <h1>{title}</h1>
-        <JsxParser jsx={html} />
+        <div className="title">{title}</div>
+        <TabSet
+          activeTabId={activeTab}
+          onTabClicked={this.handleTabClicked}
+          showTabs
+          tabs={[{
+            contents: <>
+              <JsxParser jsx={html} />
+              <ArticleChildren articles={children} />
+            </>,
+            id: 'read',
+            tab: <><ReadIcon style={{ marginRight: 5 }} /> Article</>,
+          }, {
+            contents: <WysiwygEditor html={html} onChange={this.handleChangeHtml} />,
+            id: 'edit',
+            tab: <EditIcon />,
+          }, {
+            contents: <HtmlEditor value={html} onChange={this.handleChangeHtml} />,
+            id: 'html',
+            tab: <HtmlIcon />,
+          }]}
+        />
       </div>
     )
   }
