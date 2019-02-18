80569ca4acd0bb4c757698ca565a6aa33daa39dd
Troy Alford <troy@troyalford.com>
2019-02-18 00:56:54 -0800

    ESLint

--- projects/wiki/components/Layout/Layout.js
+++ projects/wiki/components/Layout/Layout.js
@@ -11,22 +11,24 @@ export default class Layout extends Component {
   render = () => {
     const { name, theme } = this.context
 
-    return <>
-      <Head>
-        <title>{name}</title>
-        <link
-          key="content-font"
-          href={`//fonts.googleapis.com/css?family=${theme.fontFamily}:400,400i,700,700i`}
-          rel="stylesheet"
-        />
-      </Head>
-      <div className="wiki layout">
-        <Header />
-        <div className="content">
-          <Navigation />
-          {this.props.children}
+    return (
+      <>
+        <Head>
+          <title>{name}</title>
+          <link
+            key="content-font"
+            href={`//fonts.googleapis.com/css?family=${theme.fontFamily}:400,400i,700,700i`}
+            rel="stylesheet"
+          />
+        </Head>
+        <div className="wiki layout">
+          <Header />
+          <div className="content">
+            <Navigation />
+            {this.props.children}
+          </div>
         </div>
-      </div>
-    </>
+      </>
+    )
   }
 }
