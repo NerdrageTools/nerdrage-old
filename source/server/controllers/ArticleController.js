5c6c6476498d5493e3cb0b304b613bd157f05c5d
Troy Alford <troy@troyalford.com>
2019-02-18 22:21:28 -0800

    Article getter with fallback

--- source/server/controllers/ArticleController.js
+++ source/server/controllers/ArticleController.js
@@ -1,12 +1,31 @@
 import express from 'express'
-import Article from '../models/Article'
+import Article from '@/server/models/Article'
+import Campaign from '@/server/models/Campaign'
 
 const controller = express()
 
 controller.get('/:slug', async (request, response) => {
-  const { slug } = request.params
+  const campaign = await Campaign.findOne({ domain: request.campaign })
+  if (!campaign.isVisibleTo(request.session._id)) {
+    return response.status(401).send({
+      message: `This content is private to the ${request.campaign} campaign.`,
+    })
+  }
 
-  const article = await Article.findOne({ slug })
+  const { slug } = request.params
+  let article = await Article.findOne({ $or: [
+    { domain: campaign.domain, slug },
+    { domain: undefined, slug },
+  ] }).sort({ campaign: -1 })
+    .populate('campaign', 'domain name')
+    .populate('createdBy lastUpdatedBy', 'name username')
+    .exec()
+
+  if (!article) {
+    article = new Article().toJSON()
+    delete article._id
+  }
+
   return response.status(200).json(article)
 })
 
