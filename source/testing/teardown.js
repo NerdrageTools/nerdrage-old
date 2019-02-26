bad9109b92397f7d2e984a1e7478c090669d194e
Troy Alford <troy@troyalford.com>
2019-02-26 13:53:54 -0800

    Clean-up mongodb-memory-server

--- source/testing/teardown.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-console,no-underscore-dangle */
-module.exports = async () => {
-  console.log('Teardown mongod')
-  await global.MONGOD.stop()
-}
-/* eslint-enable no-console,no-underscore-dangle */
