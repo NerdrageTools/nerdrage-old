1147a863c889d5181e2268081d67ba6a4748b373
Troy Alford <troy@troyalford.com>
2019-03-04 21:58:07 -0800

    Add Sheet model

---
+++ source/server/models/Sheet.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+import Slug from '@/server/models/Slug'
+
+const { ObjectId: ObjectIdType } = mongoose.Schema.Types
+
+export const SheetSchema = new mongoose.Schema({
+  campaign: { ref: 'Campaign', required: true, type: ObjectIdType, unique: false },
+  data: { default: {}, type: Object },
+  slug: { ...Slug, required: true, unique: false },
+  ownedBy: { ref: 'User', type: ObjectIdType },
+  public: { default: false, type: Boolean },
+}, {
+  id: false,
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+  versionKey: 'version',
+})
+
+SheetSchema.index({ campaign: 1, slug: 1 }, { unique: true })
+
+const Sheet = mongoose.model('Sheet', SheetSchema)
+
+export default Sheet
