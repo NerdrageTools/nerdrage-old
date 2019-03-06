7ee391353fe02b11e4c3dbf507094a74a528a148
Troy Alford <troy@troyalford.com>
2019-03-06 00:38:13 -0800

    Better styling for default error page

--- source/pages/_error.js
+++ source/pages/_error.js
@@ -1,9 +1,11 @@
+import NextError from 'next/error'
 import React from 'react'
+import './_error.scss'
 
 export default function Error(props) {
   return (
-    <div className="error">
-      {JSON.stringify(props)}
+    <div className="error page">
+      <NextError {...props} />
     </div>
   )
 }
