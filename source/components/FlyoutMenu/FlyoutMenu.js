ff63601a11f6d142b29f3c7a6b512d9d24433200
Troy Alford <troy@troyalford.com>
2019-03-15 17:21:14 -0700

    Add FlyoutMenu component & styles

---
+++ source/components/FlyoutMenu/FlyoutMenu.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import MenuIcon from '@/icons/menu.svg'
+import noop from '@/utilities/noop'
+import './FlyoutMenu.scss'
+
+export default class FlyoutMenu extends Component {
+  static defaultProps = {
+    children: [],
+    className: '',
+    icon: MenuIcon,
+    open: undefined,
+  }
+
+  state = {
+    open: this.props.open !== undefined ? this.props.open : false,
+  }
+
+  close = () => this.setState({ open: false })
+  open = () => this.setState({ open: true })
+  toggleOpen = () => this.setState({ open: !this.state.open })
+
+  render = () => {
+    const { children, className, icon: Icon } = this.props
+    const open = this.props.open !== undefined ? this.props.open : this.state.open
+
+    return (
+      <div className={`${className} flyout menu ${open ? 'open' : 'closed'}`}>
+        {open && <div className="flyout-panel">{children}</div>}
+        <div className="menu-toggler" onClick={this.toggleOpen}><Icon /></div>
+      </div>
+    )
+  }
+}
+
+export function MenuItem({
+  children = null,
+  className = '',
+  onClick = noop,
+}) {
+  const clickable = onClick !== noop
+  const classNames = [
+    className,
+    'menu-item',
+    clickable ? 'clickable' : '',
+  ].filter(Boolean).join(' ')
+
+  return (
+    <div className={classNames} onClick={onClick}>
+      {children}
+    </div>
+  )
+}
