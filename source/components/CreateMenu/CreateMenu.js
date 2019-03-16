6c0d3f13165259476523ab96ab162e934b4297fb
Troy Alford <troy@troyalford.com>
2019-03-16 02:08:45 -0700

    Add icons & add sheet action

--- source/components/CreateMenu/CreateMenu.js
+++ source/components/CreateMenu/CreateMenu.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react'
 import FlyoutMenu, { MenuItem } from '@/components/FlyoutMenu'
 import Application from '@/contexts/Application'
+import ArticleIcon from '@/icons/read.svg'
+import SheetIcon from '@/icons/sheet.svg'
 import prompt from '@/utilities/prompt'
 import { slugify } from '@/utilities/slugs'
 
 export default class CreateMenu extends Component {
   static contextType = Application
 
-  handleNewArticle = async () => {
+  flyoutMenu = React.createRef()
+
+  handleNew = type => async () => {
+    this.flyoutMenu.current.close()
+
+    const lowered = type.toLowerCase()
     try {
-      const title = await prompt('What would you like to name your new article?', {
-        defaultValue: 'New Article',
-        title: 'Article Name',
+      const title = await prompt(`What would you like to name your new ${lowered}?`, {
+        defaultValue: `New ${type}`,
+        title: `${type} Name`,
       })
       const slug = slugify(await prompt('What would you like the slug to be?', {
         defaultValue: slugify(title),
         regex: /^[a-z0-9-]*$/i,
-        title: 'Article Slug',
+        title: `${type} Slug`,
       }))
 
       this.context.router.push(
-        `/article?slug=${slug}&title=${title}`,
-        `/article/${slug}?title=${title}`
+        `/${lowered}?slug=${slug}&title=${title}`,
+        `/${lowered}/${slug}?title=${title}`
       )
     } catch (error) { }
   }
@@ -29,8 +36,13 @@ export default class CreateMenu extends Component {
   render = () => {
     const { children } = this.props
     return (
-      <FlyoutMenu className="create" open>
-        <MenuItem onClick={this.handleNewArticle}>New Article...</MenuItem>
+      <FlyoutMenu className="create" ref={this.flyoutMenu}>
+        <MenuItem onClick={this.handleNew('Article')}>
+          <ArticleIcon /> New Article...
+        </MenuItem>
+        <MenuItem onClick={this.handleNew('Sheet')}>
+          <SheetIcon /> New Sheet...
+        </MenuItem>
         <hr />
         {children}
       </FlyoutMenu>
