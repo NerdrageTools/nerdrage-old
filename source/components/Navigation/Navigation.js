657c510bc6af8cce40e8a23aa332d1c771802854
Troy Alford <troy@troyalford.com>
2019-03-19 00:53:29 -0700

    Only show local favorites/sheets

--- source/components/Navigation/Navigation.js
+++ source/components/Navigation/Navigation.js
@@ -18,25 +18,26 @@ export default class Navigation extends Component {
     Boolean(list.length) && <>
       <b>{listTitle}</b>
       <ul className="favorites">
-        {list.map(({ campaign = {}, slug, title }, key) => {
-          const { domain = '', title: cTitle = '' } = campaign
-          let text = title
-          if (type !== 'campaign' && domain && domain !== this.context.domain) {
-            text += ` (${cTitle || domain})`
-          }
+        {list.filter(item => item.campaign.domain === this.context.campaign.domain)
+          .map(({ campaign = {}, slug, title }, key) => {
+            const { domain = '', title: cTitle = '' } = campaign
+            let text = title
+            if (type !== 'campaign' && domain && domain !== this.context.domain) {
+              text += ` (${cTitle || domain})`
+            }
 
-          return (
-            <li key={key}>
-              <ArticleLink
-                {...{ campaign, slug, type }}
-                active={this.context.domain === domain && this.context.router.asPath === `/${type}/${slug}`}
-                onClick={this.props.onItemClick}
-              >
-                {text}
-              </ArticleLink>
-            </li>
-          )
-        })}
+            return (
+              <li key={key}>
+                <ArticleLink
+                  {...{ campaign, slug, type }}
+                  active={this.context.domain === domain && this.context.router.asPath === `/${type}/${slug}`}
+                  onClick={this.props.onItemClick}
+                >
+                  {text}
+                </ArticleLink>
+              </li>
+            )
+          })}
       </ul>
     </>
   )
@@ -51,15 +52,12 @@ export default class Navigation extends Component {
       ...link,
       campaign: pluck(campaign, '_id', 'domain', 'title'),
     }))
-    // eslint-disable-next-line no-shadow
-    const campaigns = (user.campaigns || []).map(campaign => ({ campaign, title: campaign.title }))
 
     return (
       <Scrollbars className="navigation" autoHide universal>
         <div className="content" ref={this.props.wrapperRef}>
           {this.renderList(navigation, campaign.title)}
           {user && <>
-            {this.renderList(campaigns, 'My Campaigns', 'campaign')}
             {this.renderList(favorites, 'My Favorites')}
             {this.renderList(sheets, 'My Sheets', 'sheet')}
           </>}
