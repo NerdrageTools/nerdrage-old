28accb81f2a70a360f4feadb5c5b634f2cf96139
Troy Alford <troy@troyalford.com>
2019-03-22 10:49:58 -0700

    Controller should report sheet-level permissions

--- source/server/controllers/SheetController.js
+++ source/server/controllers/SheetController.js
@@ -2,38 +2,45 @@ import express from 'express'
 import Sheet from '@/server/models/Sheet'
 import omit from '@/utilities/omit'
 
-const getSheet = async (slug, campaign) => Sheet.findOne({ campaign, slug })
-  .populate('campaign', 'domain title')
-  .populate('ownedBy', 'name username')
-  .exec()
+const getSheet = async (slug, campaign, user) => {
+  const sheet = await Sheet.findOne({ campaign, slug })
+    .populate('campaign', 'domain title')
+    .populate('ownedBy', 'name username')
+    .exec()
+
+  const isOwner = !sheet || sheet.ownedBy === user._id || campaign.isOwner
+  const isEditor = isOwner || campaign.isEditor
+  const isVisible = isOwner || (campaign.isViewer && sheet && !sheet.secret)
+
+  return {
+    ...(sheet ? sheet.toJSON() : {}),
+    isEditor,
+    isOwner,
+    isVisible,
+  }
+}
 
 export const permissions = (...required) => async (request, response, next) => {
   const { campaign, domain, params: { slug }, user } = request
-  const sheet = await getSheet(slug, campaign._id)
+  const sheet = await getSheet(slug, campaign._id, user)
 
-  const isOwner = campaign.isOwner || sheet.ownedBy === user._id
-  const isEditor = isOwner || campaign.isEditor
-  const isVisible = isOwner || (campaign.isViewer && !sheet.secret)
-
-  if (sheet && required.includes('view') && !isVisible) {
+  if (sheet && required.includes('view') && !sheet.isVisible) {
     return response.status(401).json({
       message: `This content is private to the ${domain} campaign.`,
     })
   }
-  if (sheet && required.includes('edit') && !isEditor) {
+  if (sheet && required.includes('edit') && !sheet.isEditor) {
     return response.status(401).json({
       message: `You do not have permission to edit the ${domain} campaign.`,
     })
   }
-  if (sheet && sheet.secret && !isEditor) {
+  if (sheet && sheet.secret && !sheet.isEditor) {
     return response.status(401).json({
       message: `This sheet is secret! Only its owner and owners of the ${campaign.domain} campaign can see or edit it.`,
     })
   }
 
   Object.assign(request, {
-    isEditor,
-    isOwner,
     sheet,
     slug,
   })
@@ -41,21 +48,18 @@ export const permissions = (...required) => async (request, response, next) => {
   return next()
 }
 export const getSheetRequest = async (request, response) => {
-  const { domain, isEditor, isOwner, sheet, slug } = request
-  if (!sheet) {
+  const { campaign, domain, sheet, slug } = request
+  if (!sheet._id && !campaign.isParticipant) {
     return response.status(404).json({
+      ...sheet,
       message: `Unable to find sheet '${slug}' in the ${domain} campaign.`,
     })
   }
 
-  return response.status(200).json({
-    ...sheet.toJSON(),
-    isEditor,
-    isOwner,
-  })
+  return response.status(200).json(sheet)
 }
 export const upsertSheet = async (request, response) => {
-  const { body, campaign, isEditor, isOwner, slug, user } = request
+  const { body, campaign, slug, user } = request
   let { sheet } = request
   const creating = !sheet
 
@@ -75,12 +79,12 @@ export const upsertSheet = async (request, response) => {
   }
 
   await sheet.save()
-  const saved = await getSheet(slug, campaign._id)
+  const saved = await getSheet(slug, campaign._id, user)
 
   return response.status(200).json({
     ...saved.toJSON(),
-    isEditor: creating ? true : isEditor,
-    isOwner: creating ? true : isOwner,
+    isEditor: creating ? true : sheet.isEditor,
+    isOwner: creating ? true : sheet.isOwner,
   })
 }
 export const deleteSheet = async (request, response) => {
