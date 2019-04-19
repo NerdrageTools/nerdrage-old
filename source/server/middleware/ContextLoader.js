a152a65a1181c1a985f6f25bdd051138668dc01b
Troy Alford <troy@troyalford.com>
2019-04-19 12:54:12 -0700

    Fix host issue with cookies

--- source/server/middleware/ContextLoader.js
+++ source/server/middleware/ContextLoader.js
@@ -2,10 +2,6 @@ import { getCampaign } from '@/server/controllers/CampaignController'
 import { getUser } from '@/server/controllers/UserController'
 
 export default async (request, response, next) => {
-  const host = request.get('host') || ''
-  const sessionScope = host.split('.').slice(-2).join('.').split(':')[0]
-  request.sessionOptions.subdomain = sessionScope
-
   if (request.session && !request.session.anonymous) {
     const user = await getUser(request.session.username, true)
     if (!user.anonymous) {
