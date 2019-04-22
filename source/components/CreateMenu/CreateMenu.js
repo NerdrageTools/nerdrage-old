c8c2ea69558bf0d6e2e9e65cee4f9e99a3800652
Troy Alford <troy@troyalford.com>
2019-04-22 12:10:35 -0700

    Support creating Campaigns

--- source/components/CreateMenu/CreateMenu.js
+++ source/components/CreateMenu/CreateMenu.js
@@ -3,6 +3,7 @@ import FlyoutMenu, { MenuItem } from '@/components/FlyoutMenu'
 import Application from '@/contexts/Application'
 import ArticleIcon from '@/icons/read.svg'
 import SheetIcon from '@/icons/sheet.svg'
+import alert from '@/utilities/alert'
 import promptLink from '@/utilities/promptLink'
 import './CreateMenu.scss'
 
@@ -20,7 +21,7 @@ export default class CreateMenu extends Component {
         defaultSlug: `new-${lowered}`,
         defaultText: `New ${type}`,
         templateType: type,
-        title: `New ${lowered}...`,
+        title: `New ${type}...`,
       })
 
       this.context.router.push(
@@ -29,13 +30,45 @@ export default class CreateMenu extends Component {
       )
     } catch (error) { }
   }
+  handleNewCampaign = async () => {
+    const { rootUrl } = this.context
+    this.flyoutMenu.current.close()
+
+    try {
+      const { slug, text } = await promptLink({
+        defaultSlug: 'new-campaign',
+        defaultText: 'New Campaign',
+        slugLabel: 'Subdomain',
+        textLabel: 'Title',
+        title: 'New Campaign...',
+      })
+
+      const response = await fetch(`/api/campaign/${slug}`, {
+        body: JSON.stringify({ subdomain: slug, title: text }),
+        credentials: 'include',
+        method: 'PUT',
+      })
+      if (response.status === 201) {
+        this.context.router.push(`//${slug}.${rootUrl}/campaign`, `//${slug}.${rootUrl}/campaign`)
+      } else {
+        const json = await response.json()
+        alert(json.message, 'Error')
+      }
+    } catch (error) { }
+  }
 
   render = () => {
     const { children } = this.props
     const { isEditor, isParticipant } = this.context.campaign || {}
+    const { isAdmin } = this.context.user || {}
 
     return (
       <FlyoutMenu className="create" ref={this.flyoutMenu}>
+        {isAdmin && (
+          <MenuItem onClick={this.handleNewCampaign}>
+            <SheetIcon /> New Campaign...
+          </MenuItem>
+        )}
         {isEditor && (
           <MenuItem onClick={this.handleNew('Article')}>
             <ArticleIcon /> New Article...
