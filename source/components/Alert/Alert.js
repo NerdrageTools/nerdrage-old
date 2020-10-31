f670437a016a2b9a9cfb6b379e1ba0992798b76a
Troy Alford <talford@vestmark.com>
2020-10-31 14:39:57 -0700

    Convert to TS

--- source/components/Alert/Alert.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import './Alert.scss'
-
-export function Alert({ className = '', children = [], type = 'info' }) {
-	return <div className={`alert ${type} ${className}`}>{children}</div>
-}
---
+++ source/components/Alert/Alert.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type TProps = {
+	className: string,
+}
+
+export class Alert extends React.Component<TProps> {
+	static styles = import('./Alert.scss')
+	render = (): JSX.Element => (
+		<div className={`alert ${this.props.className}`}>
+			{this.props.children}
+		</div>
+	)
+}
