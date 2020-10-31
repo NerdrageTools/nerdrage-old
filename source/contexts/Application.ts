186a2cbc9d9cee1fe85d27857bcc075dd0a3b452
Troy Alford <talford@vestmark.com>
2020-10-31 13:22:44 -0700

    Improve typing

--- source/contexts/Application.ts
+++ source/contexts/Application.ts
@@ -1,3 +1,5 @@
+import { Router } from 'next/router'
+import { noop } from '@/utilities/noop'
 import React from 'react'
 
 const CAMPAIGN_DEFAULTS = {
@@ -24,7 +26,33 @@ const USER_DEFAULTS = {
 	anonymous: true,
 }
 
-export const Application = React.createContext({
+type TContext = {
+	campaign: typeof CAMPAIGN_DEFAULTS,
+	logOff: typeof noop,
+	rootUrl: string,
+	router?: Router,
+	setCampaign: typeof noop,
+	setSize: typeof noop,
+	setUser: typeof noop,
+	size: 'small' | 'medium' | 'large',
+	subdomain: string,
+	theme: typeof CAMPAIGN_DEFAULTS.theme,
+	updateCampaign: typeof noop,
+	updateUser: typeof noop,
+	user: typeof USER_DEFAULTS,
+}
+
+export const Application = React.createContext<TContext>({
 	campaign: CAMPAIGN_DEFAULTS,
+	logOff: noop,
+	rootUrl: '',
+	setCampaign: noop,
+	setSize: noop,
+	setUser: noop,
+	size: 'large',
+	subdomain: '',
+	theme: CAMPAIGN_DEFAULTS.theme,
+	updateCampaign: noop,
+	updateUser: noop,
 	user: USER_DEFAULTS,
 })
