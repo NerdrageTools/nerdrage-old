c4db312ecefbf0c1695d0f78ba6d1c70d20737d2
Troy Alford <talford@vestmark.com>
2020-11-22 16:49:06 -0800

    Move Article Search logic to Article

--- source/server/controllers/SearchController.ts
+++ source/server/controllers/SearchController.ts
@@ -1,11 +1,8 @@
 import deburr from 'lodash.deburr'
-import * as entities from 'entities'
 import express, { IRequest, Response } from 'express'
 import fetch from 'isomorphic-unfetch'
-import { Campaign404 } from '~/server/errors/Campaign404'
-import { Campaign, IArticleSearchResult, Users } from '~/server/models'
+import { Articles, Campaign, Users } from '~/server/models'
 import { IFontSearchResult } from '~/server/schema/IFont'
-import { loadByCampaign } from '~/server/utilities/loadByCampaign'
 import { bound } from '~/utilities/bound'
 
 interface ISearchRequest extends IRequest {
@@ -13,10 +10,6 @@ interface ISearchRequest extends IRequest {
 	allowPublic: boolean,
 	params: { searchTerm: string },
 	query: { limit: string },
-}
-interface ITextMatch {
-	idx: number,
-	match: string,
 }
 
 const FONTS = fetch('https://fonts.google.com/metadata/fonts').then(async response => {
@@ -29,74 +22,18 @@ const FONTS = fetch('https://fonts.google.com/metadata/fonts').then(async respon
 	return [] as IFontSearchResult[]
 })
 
-interface IArticleResult extends IArticleSearchResult {
-	nameMatches: ITextMatch[],
-	plainText: string,
-	textMatches: ITextMatch[],
-}
-export const searchArticles = (
-	async (request: ISearchRequest, response: Response): Promise<Response> => {
-		const { campaign, subdomain, user } = request
-		if (!campaign) {
-			Campaign404({ campaign, subdomain }, response)
-			return response
-		}
+export async function searchArticles(request: ISearchRequest, response: Response): Promise<void> {
+	const { params: { searchTerm }, subdomain, user } = request
+	const results = await Articles.search(searchTerm, subdomain, user?._id)
 
-		const userIsViewer = Boolean(user?.isAdmin || campaign.isViewer)
-		const userIsOwner = Boolean(user?.isAdmin || campaign.isOwner)
-		if (!userIsViewer) {
-			return response.status(401).send({
-				message: 'You are not authorized to view articles in this campaign.',
-			})
-		}
-
-		const words = deburr(entities.decodeHTML(request.params.searchTerm ?? ''))
-			.split(/\b/)
-			.map(word => word.trim()).filter(Boolean)
-		const regex = new RegExp(`(?<match>${words.join('|')})`, 'gim')
-		const projection = { campaign: 1, plainText: 1, secret: 1, slug: 1, template: 1, title: 1 }
-		const articles = await loadByCampaign<IArticleResult>('Article', campaign, {
-			aggregation: [
-				{ $project: {
-					...projection,
-					slugMatches: { $regexFindAll: { input: '$slug', regex } },
-					textMatches: { $regexFindAll: { input: '$plainText', regex } },
-					titleMatches: { $regexFindAll: { input: '$title', regex } },
-				} },
-				{ $addFields: { aliasMatches: { $regexFindAll: { input: '$aliases', regex } } } },
-				{ $project: {
-					...projection,
-					// eslint-disable-next-line max-len
-					nameHits: { $size: { $concatArrays: ['$slugMatches', '$aliasMatches', '$titleMatches'] } },
-					nameMatches: { $concatArrays: ['$slugMatches', '$aliasMatches', '$titleMatches'] },
-					textHits: { $size: '$textMatches' },
-					textMatches: 1,
-				} },
-				{ $match: {
-					$or: [{ nameHits: { $gt: 0 } }, { textHits: { $gt: 0 } }],
-					...(!userIsOwner ? { secret: false } : {}),
-				} },
-				{ $sort: { nameHits: -1, textHits: -1 } },
-				{ $limit: bound(parseInt(request.query.limit, 10), { min: 0 }) },
-			],
-			castAsModel: false,
+	if (results === null) {
+		response.status(401).send({
+			message: 'You are not authorized to view articles in this campaign.',
 		})
-
-		const results = articles.map<IArticleSearchResult>(
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			({ nameMatches, plainText, textMatches, ...article }) => ({
-				...article,
-				preview: textMatches.map(({ idx, match }: ITextMatch) => [
-					`...${plainText.substr(idx - 25, 25)}`,
-					`<span class="highlight">${match}</span>`,
-					`${plainText.substr(idx + match.length, 25)}`,
-				].join('')),
-			}),
-		)
-
-		return response.status(200).json(results)
+	} else {
+		response.status(200).json(results)
 	}
-)
+}
 export const searchCampaigns = (
 	async (request: ISearchRequest, response: Response): Promise<Response> => {
 		const $regex = new RegExp(deburr(request.params.searchTerm ?? '.'), 'i')
@@ -132,9 +69,9 @@ export const searchFonts = (
 		return response.status(200).json(matches)
 	}
 )
-export async function searchUsers(request: ISearchRequest, response: Response): Promise<Response> {
+export async function searchUsers(request: ISearchRequest, response: Response): Promise<void> {
 	const $regex = new RegExp(`^${deburr(request.params.searchTerm ?? '.')}`, 'i')
-	return response.status(200).json(await Users.search($regex))
+	response.status(200).json(await Users.search($regex))
 }
 
 export default express()
--- source/server/models/Article.ts
+++ source/server/models/Article.ts
@@ -4,6 +4,7 @@ import {
 import cheerio from 'cheerio'
 import * as entities from 'entities'
 import { html_beautify as beautify } from 'js-beautify'
+import deburr from 'lodash.deburr'
 import { Slug } from '~/server/models/Slug'
 import { computeSearchKeys } from '~/utilities/computeSearchKeys'
 import { createCampaignFilter } from '~/utilities/createCampaignFilter'
@@ -12,7 +13,9 @@ import { unique } from '~/utilities/unique'
 import { cleanUp } from '~/server/models/Article/cleanUp'
 import { renderLinks } from '~/server/models/Article/renderLinks'
 import { Campaign } from './Campaign'
-import { User } from './User'
+import { User, Users } from './User'
+import { loadByCampaign } from '../utilities/loadByCampaign'
+import { bound } from '~/utilities/bound'
 
 const slugifyArray = (array: string[]) => unique(array.map(value => (
 	value.toLowerCase().replace(/[^\w-_]/g, '-').replace(/-{2,}|^-|-$/g, '')
@@ -42,13 +45,21 @@ export interface IArticleData {
 	updatedAt?: Date,
 	version?: number,
 }
-
 export interface IArticleLink {
 	slug: string,
 	subdomain: string,
 	title: string,
 }
 
+interface ITextMatch {
+	idx: number,
+	match: string,
+}
+interface IArticleResult extends IArticleSearchResult {
+	nameMatches: ITextMatch[],
+	plainText: string,
+	textMatches: ITextMatch[],
+}
 export interface IArticleSearchResult {
 	nameHits: number,
 	preview: string[],
@@ -144,4 +155,74 @@ export class Article implements IArticleData {
 	}
 }
 
-export const Articles = getModelForClass(Article)
+const search = async (
+	searchTerm: string | RegExp,
+	subdomain: string,
+	userId: string = '',
+	limit: number = 10,
+): Promise<IArticleSearchResult[] | null> => {
+	const campaign = subdomain ? await Campaign.findOne({ subdomain }) : null
+	if (!campaign) return null
+
+	const user = userId ? await Users.findOne({ _id: userId }) : null
+	const userIsViewer = Boolean(user?.isAdmin || campaign.isVisibleTo(userId))
+	if (!userIsViewer) return null
+
+	const userIsOwner = Boolean(user?.isAdmin || campaign.isOwnedBy(userId))
+
+	let regex: RegExp
+	if (typeof searchTerm === 'string') {
+		const words = deburr(entities.decodeHTML(searchTerm ?? ''))
+			.split(/\b/)
+			.map(word => word.trim()).filter(Boolean)
+		regex = new RegExp(`(?<match>${words.join('|')})`, 'gim')
+	} else {
+		regex = searchTerm
+	}
+	const projection = { campaign: 1, plainText: 1, secret: 1, slug: 1, template: 1, title: 1 }
+	const articles = await loadByCampaign<IArticleResult>('Article', campaign, {
+		aggregation: [
+			{ $project: {
+				...projection,
+				slugMatches: { $regexFindAll: { input: '$slug', regex } },
+				textMatches: { $regexFindAll: { input: '$plainText', regex } },
+				titleMatches: { $regexFindAll: { input: '$title', regex } },
+			} },
+			{ $addFields: { aliasMatches: { $regexFindAll: { input: '$aliases', regex } } } },
+			{ $project: {
+				...projection,
+				// eslint-disable-next-line max-len
+				nameHits: { $size: { $concatArrays: ['$slugMatches', '$aliasMatches', '$titleMatches'] } },
+				nameMatches: { $concatArrays: ['$slugMatches', '$aliasMatches', '$titleMatches'] },
+				textHits: { $size: '$textMatches' },
+				textMatches: 1,
+			} },
+			{ $match: {
+				$or: [{ nameHits: { $gt: 0 } }, { textHits: { $gt: 0 } }],
+				...(!userIsOwner ? { secret: false } : {}),
+			} },
+			{ $sort: { nameHits: -1, textHits: -1 } },
+			{ $limit: bound(limit, { min: 0 }) },
+		],
+		castAsModel: false,
+	})
+
+	return articles.map<IArticleSearchResult>(
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		({ nameMatches, plainText, textMatches, ...article }) => ({
+			...article,
+			preview: textMatches.map(({ idx, match }: ITextMatch) => [
+				`...${plainText.substr(idx - 25, 25)}`,
+				`<span class="highlight">${match}</span>`,
+				`${plainText.substr(idx + match.length, 25)}`,
+			].join('')),
+		}),
+	)
+}
+
+interface IArticles extends ReturnModelType<typeof Article> {
+	search: typeof search,
+}
+export const Articles: IArticles = Object.assign(getModelForClass(Article), {
+	search,
+})
